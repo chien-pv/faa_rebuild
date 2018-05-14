a40ef2d64e501c72cb04ee515805b4b315903499
vannam1996 <vannam051096@gmail.com>
2018-05-14 21:12:48 +0700

    reason

--- app/serializers/registration_serializer.rb
+++ app/serializers/registration_serializer.rb
@@ -1,5 +1,6 @@
 class RegistrationSerializer < ActiveModel::Serializer
-  attributes :id, :name, :email, :phone, :address, :course_name, :comment, :course_schedule_code, :created_at
+  attributes :id, :name, :email, :phone, :address, :course_name, :comment,
+    :course_schedule_code, :created_at, :reason_name
   has_one :course
   has_one :course_schedule
 
@@ -9,5 +10,14 @@ class RegistrationSerializer < ActiveModel::Serializer
 
   def course_schedule_code
     object.course_schedule.code
+  end
+
+  def reason_name
+    full_name = ""
+    object.reason.each do |reason_id|
+      full_name += Registration.list_reasons.keys[reason_id&.to_i]
+      full_name += ", " if object.reason.last != reason_id
+    end
+    full_name
   end
 end
